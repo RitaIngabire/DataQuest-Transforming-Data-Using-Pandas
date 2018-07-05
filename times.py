2e8b2d5fef40f833daf0581ed7b31f65b3ee4ad7
Rita Ingabire <ritah.ingabire@gmail.com>
2018-07-05 11:56:23 +0300

    Update times.py

--- times.py
+++ times.py
@@ -2,8 +2,8 @@ import read
 from dateutil.parser import parse
 df = read.load_data()
 
-df['submission_time'].apply(lambda x: print(parse(x).hour))
-
+#extract hour from the date time column
 df['hour'] = df['submission_time'].apply(lambda x: parse(x).hour)
 
+#How often does each hour make an appearance 
 print(df['hour'].value_counts())
